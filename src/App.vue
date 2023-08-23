a9dbf1eec518fab8ed79c4940efd2e29ada90d66
RahulPawar1212 <rahul.pawar@nepa.com>
2023-08-23 21:22:40 +0530

    First commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+
+<template>
+
+<Chiptest/>
+</template>
+
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Chiptest from './components/chiptest.vue';
+
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>Test</div>
+    <div>
+      <textarea v-model="inputData" @keypress="handleText(inputData)"></textarea>
+      <button @click="handleText('Test')">Say hello</button>
+      <div>
+        <h3>Converted List:</h3>
+  
+        <h1>test</h1>
+  
+        <v-chip closable v-for="(item, index) in itemList" :key="index">{{
+          item
+        }}</v-chip>
+        <h1>test 2</h1>
+        <div>{{ inputData }}</div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        inputData: "",
+        itemList: [],
+        name: "HelloWorld",
+      };
+    },
+    methods: {
+      handlePaste(event) {
+        const clipboardData = event.clipboardData || window.clipboardData;
+        const pastedData = clipboardData.getData("text");
+  
+        // Split pasted data by newline to create the list of items
+        const items = pastedData.split(",").filter((item) => item.trim() !== "");
+        this.itemList = items;
+      },
+      handleText(event) {
+        //const clipboardData = event.clipboardData || window.clipboardData;
+        //const pastedData = clipboardData.getData("text");
+        console.log(event);
+        // Split pasted data by newline to create the list of items
+         const items = event.split(",").filter((item) => item.trim() !== "");
+         console.log(items);
+        //debug.log(items);
+        // this.itemList = items;
+      },
+      showAlert: (message) => {
+        alert(message);
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  /* Add your component's styles here */
+  </style>
+  
